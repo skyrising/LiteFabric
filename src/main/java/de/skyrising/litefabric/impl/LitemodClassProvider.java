637676a147de294f8cb8b61f468dc15b8e2c7f47
Simon Wanner <skyrising@pvpctutorials.de>
2021-04-18 13:25:40 +0200

    Fix non-public overwrite methods in dev

--- src/main/java/de/skyrising/litefabric/impl/LitemodClassProvider.java
+++ src/main/java/de/skyrising/litefabric/impl/LitemodClassProvider.java
@@ -1,21 +1,30 @@
 package de.skyrising.litefabric.impl;
 
+import net.fabricmc.loader.api.FabricLoader;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.commons.ClassRemapper;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.util.Annotations;
+import org.spongepowered.asm.util.Bytecode;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.URL;
 import java.nio.file.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class LitemodClassProvider {
     private static final boolean DUMP = true;
 
     private final FileSystem fileSystem;
     private final LitemodRemapper remapper;
+    private final Map<String, byte[]> classByteCache = new HashMap<>();
 
     public LitemodClassProvider(FileSystem fileSystem, LitemodRemapper remapper) {
         this.fileSystem = fileSystem;
@@ -23,6 +32,8 @@ class LitemodClassProvider {
     }
 
     byte[] getClassBytes(String name) {
+        byte[] cached = classByteCache.get(name);
+        if (cached != null) return cached;
         String classFileName = name.replace('.', '/') + ".class";
         Path classFilePath = fileSystem.getPath(classFileName);
         if (!Files.exists(classFilePath)) return null;
@@ -40,8 +51,19 @@ class LitemodClassProvider {
         ClassNode remapped = new ClassNode();
         ClassRemapper clsRemapper = new ClassRemapper(remapped, remapper);
         raw.accept(clsRemapper);
+        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
+            if (Annotations.getInvisible(remapped, Mixin.class) != null) {
+                for (MethodNode method : remapped.methods) {
+                    if ((method.access & Opcodes.ACC_STATIC) != 0) continue;
+                    if (method.visibleAnnotations == null || Annotations.getVisible(method, Overwrite.class) != null) {
+                        Bytecode.setVisibility(method, Opcodes.ACC_PUBLIC);
+                    }
+                }
+            }
+        }
         remapped.accept(writer);
         byte[] bytes = writer.toByteArray();
+        classByteCache.put(name, bytes);
         if (DUMP) {
             Path out = Paths.get(".litefabric.out/class/" + classFileName);
             try {
